17623eee9fc0238ccf4743f6a36a725ef8762c1d
alfygitau <alfygitau@gmail.com>
2025-03-30 16:30:13 +0300

    changes

---
+++ lib/pages/courses/course_units.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:my_school/pages/homepage/landing.dart';
+import 'package:my_school/pages/notifications/notifications.dart';
+
+class CourseUnits extends StatefulWidget {
+  const CourseUnits({super.key});
+
+  @override
+  State<CourseUnits> createState() => _CourseUnitsState();
+}
+
+class _CourseUnitsState extends State<CourseUnits> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFEEF2FA),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color(0xFFEEF2FA),
+        elevation: 0,
+        title: Row(
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => const Landing(),
+                  ),
+                );
+              },
+              child: Image.asset('assets/images/logo.png', height: 30),
+            ),
+            const SizedBox(width: 8),
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Raha',
+                  style: TextStyle(
+                    color: Color(0xFF1F242C),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  "School",
+                  style: TextStyle(
+                    color: Color(0xFF2D70E2),
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: Image.asset('assets/images/setting-2.png', height: 30),
+            onPressed: () {},
+          ),
+          Stack(
+            children: [
+              IconButton(
+                icon: Image.asset('assets/images/notification.png', height: 30),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => const Notifications(),
+                    ),
+                  );
+                },
+              ),
+              Positioned(
+                right: 8,
+                top: 8,
+                child: Container(
+                  padding: const EdgeInsets.all(4),
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Text(
+                    '3',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/courses/courses.dart
+++ lib/pages/courses/courses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_school/pages/courses/course_units.dart';
 import 'package:my_school/pages/homepage/landing.dart';
 import 'package:my_school/pages/notifications/notifications.dart';
 
@@ -11,26 +12,26 @@ class Courses extends StatefulWidget {
 
 class _CoursesState extends State<Courses> {
   final List<Map<String, String>> courses = [
-    {"sn": "1", "code": "CS101", "title": "Introduction to Computer Science"},
-    {"sn": "2", "code": "ENG201", "title": "Advanced English Composition"},
-    {"sn": "3", "code": "MATH310", "title": "Calculus and Linear Algebra"},
-    {"sn": "4", "code": "BIO150", "title": "Fundamentals of Biology"},
-    {"sn": "5", "code": "ECON220", "title": "Microeconomics Principles"},
-    {"sn": "6", "code": "HIST105", "title": "World History Overview"},
-    {"sn": "7", "code": "PHYS210", "title": "Physics for Engineers"},
-    {"sn": "8", "code": "CHEM120", "title": "Organic Chemistry"},
-    {"sn": "9", "code": "PSYCH101", "title": "Introduction to Psychology"},
-    {"sn": "10", "code": "SOC200", "title": "Sociology and Society"},
-    {"sn": "11", "code": "BUS310", "title": "Business Management Principles"},
-    {"sn": "12", "code": "LAW150", "title": "Introduction to Law"},
-    {"sn": "13", "code": "MED101", "title": "Basic Human Anatomy"},
-    {"sn": "14", "code": "PHIL205", "title": "Ethics and Philosophy"},
-    {"sn": "15", "code": "STAT250", "title": "Statistical Analysis"},
-    {"sn": "16", "code": "ART300", "title": "Modern Art History"},
-    {"sn": "17", "code": "LIT220", "title": "Shakespearean Literature"},
-    {"sn": "18", "code": "IT400", "title": "Cybersecurity and Data Privacy"},
-    {"sn": "19", "code": "ENV210", "title": "Environmental Science"},
-    {"sn": "20", "code": "GEOG110", "title": "Geography and Earth Studies"},
+    {"sn": "1", "code": "CS", "title": "Introduction to Computer Science"},
+    {"sn": "2", "code": "ENG", "title": "Advanced English Composition"},
+    {"sn": "3", "code": "MATH", "title": "Calculus and Linear Algebra"},
+    {"sn": "4", "code": "BIO", "title": "Fundamentals of Biology"},
+    {"sn": "5", "code": "ECON", "title": "Microeconomics Principles"},
+    {"sn": "6", "code": "HIST", "title": "World History Overview"},
+    {"sn": "7", "code": "PHYS", "title": "Physics for Engineers"},
+    {"sn": "8", "code": "CHEM", "title": "Organic Chemistry"},
+    {"sn": "9", "code": "PSYCH", "title": "Introduction to Psychology"},
+    {"sn": "10", "code": "SOC", "title": "Sociology and Society"},
+    {"sn": "11", "code": "BUS", "title": "Business Management Principles"},
+    {"sn": "12", "code": "LAW", "title": "Introduction to Law"},
+    {"sn": "13", "code": "MED", "title": "Basic Human Anatomy"},
+    {"sn": "14", "code": "PHIL", "title": "Ethics and Philosophy"},
+    {"sn": "15", "code": "STAT", "title": "Statistical Analysis"},
+    {"sn": "16", "code": "ART", "title": "Modern Art History"},
+    {"sn": "17", "code": "LIT", "title": "Shakespearean Literature"},
+    {"sn": "18", "code": "IT", "title": "Cybersecurity and Data Privacy"},
+    {"sn": "19", "code": "ENV", "title": "Environmental Science"},
+    {"sn": "20", "code": "GEO", "title": "Geography and Earth Studies"},
   ];
 
   @override
@@ -185,30 +186,57 @@ class _CoursesState extends State<Courses> {
                           }),
                           cells: [
                             DataCell(
-                              SizedBox(
-                                width: 20,
-                                child: Text(courses[index]['sn']!),
-                              ),
-                            ),
-                            DataCell(
-                              SizedBox(
-                                width: 70,
-                                child: Text(
-                                  courses[index]['code']!,
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => const CourseUnits(),
+                                    ),
+                                  );
+                                },
+                                child: SizedBox(
+                                  width: 20,
+                                  child: Text(courses[index]['sn']!),
                                 ),
                               ),
                             ),
                             DataCell(
-                              SizedBox(
-                                width: 200,
-                                child: Text(
-                                  courses[index]['title']!.toUpperCase(),
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1,
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => const CourseUnits(),
+                                    ),
+                                  );
+                                },
+                                child: SizedBox(
+                                  width: 70,
+                                  child: Text(
+                                    courses[index]['code']!,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            DataCell(
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => const CourseUnits(),
+                                    ),
+                                  );
+                                },
+                                child: SizedBox(
+                                  width: 200,
+                                  child: Text(
+                                    courses[index]['title']!.toUpperCase(),
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
                                 ),
                               ),
