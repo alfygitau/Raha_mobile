7823692290028e11acb7c32bf915e814310ccf5f
alfygitau <alfygitau@gmail.com>
2025-03-31 21:50:10 +0300

    chnges

--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:my_school/pages/auth/login.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class Welcome extends StatefulWidget {
   const Welcome({super.key});
@@ -13,12 +14,27 @@ class _WelcomeState extends State<Welcome> {
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _requestAllPermissions();
+    });
     Timer(
       const Duration(seconds: 3),
       () => Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (BuildContext context) => const Login()),
       ),
     );
+  }
+
+  Future<void> _requestAllPermissions() async {
+    List<Permission> permissions = [
+      Permission.camera,
+      Permission.microphone,
+      Permission.storage,
+      Permission.location,
+      Permission.audio,
+    ];
+    Map<Permission, PermissionStatus> statuses = await permissions.request();
+    if (statuses.values.any((status) => status.isPermanentlyDenied)) {}
   }
 
   @override
